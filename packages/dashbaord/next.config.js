e4f4f7f083324f456e94aad43576c8a1eccf1f03
jrea <juzko.r@gmail.com>
2022-03-15 09:35:40 -0400

    chore: attempt github pages

--- packages/dashbaord/next.config.js
+++ packages/dashbaord/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/nile-js/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
