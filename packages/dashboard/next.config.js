c538f00ae63e562a0fc2cc5d6bd2e027ec829cc7
Joseph Rea <jrea@users.noreply.github.com>
2022-03-16 11:31:00 -0400

    Update next.config.js

--- packages/dashboard/next.config.js
+++ packages/dashboard/next.config.js
@@ -3,7 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/nile-js/" : null,
+  // TODO remove - for use with github pages, using vercel for now
+  // assetPrefix: isProd ? "/nile-js/" : "",
 };
 
 module.exports = nextConfig;
