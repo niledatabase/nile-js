676954058acb3ad0771b91d4bed8151e962dd995
jrea <juzko.r@gmail.com>
2022-03-21 12:02:13 -0400

    feat(dashboard): add editor, it does nothing

---
+++ packages/dashboard/components/Editor.tsx
@@ -0,0 +1,28 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { useCallback, type MutableRefObject } from "react";
+
+type EditorProps = {
+  textRef: MutableRefObject<string>;
+  height: string,
+};
+export const Editor = ({ textRef, height }: EditorProps)=>{
+  const handleChange = useCallback(
+    (value) => {
+      // don't re-render... for now
+      textRef.current = value;
+    },
+    [textRef]
+  );
+  return (
+    <CodeMirror 
+        height={height} 
+        extensions={[javascript({ jsx: true })]} 
+        onChange={handleChange}
+    />
+  );
+}
+Editor.defaultProps = {
+    height:"200px"
+}
+
