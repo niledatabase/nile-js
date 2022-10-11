3748468d7651e464273abb4733fd9b6f2f9aca85
jrea <juzko.r@gmail.com>
2022-10-11 09:33:22 -0400

    fix(react): remove Component in favor of useInstances

---
+++ packages/react/src/lib/hooks/useInstances/index.tsx
@@ -0,0 +1,31 @@
+import { useNile } from '../../../context';
+import Queries, { useQuery } from '../../queries';
+import { useInterval } from '../useInterval';
+
+type Props = {
+  useQuery?: typeof useQuery;
+  refreshInterval?: number;
+};
+
+export const useInstances = (
+  orgId: string,
+  entityType: string,
+  props?: Props
+) => {
+  const { useQuery: customUseQuery, refreshInterval } = props ?? {};
+  const nile = useNile();
+  const useQueryHook = customUseQuery ?? useQuery;
+
+  const { refetch, data, isFetching } = useQueryHook(
+    Queries.ListInstances(orgId, entityType),
+    () =>
+      nile.entities.listInstances({
+        type: entityType,
+        org: orgId,
+      })
+  );
+
+  useInterval(refetch, refreshInterval);
+
+  return { data, refetch, isFetching };
+};
