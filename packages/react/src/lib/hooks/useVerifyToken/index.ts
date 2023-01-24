0b059a35e13c5f73c593d9ab9742aef6e67ecb7f
jrea <juzko.r@gmail.com>
2023-01-24 11:09:08 -0500

    feat: add useVerifyToken

---
+++ packages/react/src/lib/hooks/useVerifyToken/index.ts
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { useNile } from '../../../context';
+
+/**
+ * A function to handle removing the `state` query param on login, in exchange for a cookie that can be used for authentication
+ * @returns an error, if there is one
+ */
+export const useVerifyToken = (): null | string => {
+  const nile = useNile();
+  const [token, setToken] = React.useState<null | string>(null);
+  const [error, setError] = React.useState<null | string>(null);
+  const url = React.useMemo(() => {
+    return new URL(window.location.href);
+  }, []);
+
+  React.useEffect(() => {
+    setError(null);
+    const state = url.searchParams.get('state');
+    if (state) {
+      setToken(state);
+    }
+  }, [url.searchParams]);
+
+  React.useEffect(() => {
+    url.searchParams.delete('state');
+    window.history.pushState(
+      { path: url.pathname },
+      document.title,
+      url.toString()
+    );
+    async function doRequest() {
+      setError(null);
+      if (token) {
+        try {
+          await fetch(
+            `${nile.config?.basePath}/auth/oidc/verify?state=${token}`
+          );
+        } catch (e) {
+          if (e instanceof Error) {
+            setError(e.message);
+          }
+        }
+      }
+    }
+    doRequest();
+  }, [nile.config?.basePath, token, url]);
+
+  return error;
+};
