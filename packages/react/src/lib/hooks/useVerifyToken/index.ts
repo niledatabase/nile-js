b582a2bb49284a908dbe5ee75e59302c7e997c2b
jrea <juzko.r@gmail.com>
2023-01-25 16:18:44 -0500

    Revert "Revert "fix(react): add success to useVerifyToken""
    This reverts commit 590bd04cc98f25df96dec1d1ba955809f7e72f4d.

--- packages/react/src/lib/hooks/useVerifyToken/index.ts
+++ packages/react/src/lib/hooks/useVerifyToken/index.ts
@@ -6,10 +6,11 @@ import { useNile } from '../../../context';
  * A function to handle removing the `state` query param on login, in exchange for a cookie that can be used for authentication
  * @returns an error, if there is one
  */
-export const useVerifyToken = (): null | string => {
+export const useVerifyToken = (): [boolean, null | string] => {
   const nile = useNile();
   const [token, setToken] = React.useState<null | string>(null);
   const [error, setError] = React.useState<null | string>(null);
+  const [success, setSuccess] = React.useState(false);
   const url = React.useMemo(() => {
     return new URL(window.location.href);
   }, []);
@@ -36,6 +37,7 @@ export const useVerifyToken = (): null | string => {
           await fetch(
             `${nile.config?.basePath}/auth/oidc/verify?state=${token}`
           );
+          setSuccess(true);
         } catch (e) {
           if (e instanceof Error) {
             setError(e.message);
@@ -46,5 +48,5 @@ export const useVerifyToken = (): null | string => {
     doRequest();
   }, [nile.config?.basePath, token, url]);
 
-  return error;
+  return [success, error];
 };
