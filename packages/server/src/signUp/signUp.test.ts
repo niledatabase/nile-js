e382e75424e01b1f4a28bb8427adf5ee2091600c
jrea <juzko.r@gmail.com>
2023-05-12 08:32:02 -0400

    fix: more tests, request correctly

--- packages/server/src/signUp/signUp.test.ts
+++ packages/server/src/signUp/signUp.test.ts
@@ -1,4 +1,5 @@
 import { Config } from '../utils/Config';
+import { FakeResponse, _fetch } from '../../test/fetch.mock';
 
 import SignUp from './signUp';
 
@@ -6,11 +7,37 @@ jest.mock('../utils/ResponseError', () => ({
   ResponseError: jest.fn(),
 }));
 
+const baseConfig = [
+  'basePath',
+  'database',
+  'tenantId',
+  'workspace',
+  'cookieKey',
+  'url',
+];
 describe('signUp', () => {
-  it('goes to the right url', () => {
-    const login = new SignUp(
+  it('has expected methods', () => {
+    const signup = new SignUp(
       new Config({ workspace: 'workspace', database: 'database' })
     );
-    expect(login.url).toEqual('/workspaces/workspace/databases/database/users');
+    expect(Object.keys(signup).sort()).toEqual([...baseConfig, 'post'].sort());
+  });
+
+  it('does a post', async () => {
+    //@ts-expect-error - test
+    global.Response = FakeResponse;
+    global.fetch = _fetch();
+    const signup = new SignUp(
+      new Config({ workspace: 'workspace', database: 'database' })
+    );
+
+    const params = {
+      body: { email: 'email', password: 'password' },
+    } as unknown as Request;
+    const res = await signup.post(params);
+    //@ts-expect-error - test
+    expect(res.config).toEqual(
+      'https://prod.thenile.dev/workspaces/workspace/databases/database/users'
+    );
   });
 });
