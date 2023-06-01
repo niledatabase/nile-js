c16e1f466128a92f7b5d933da03b8d78cd7daa0c
Mki Pokryvailo <mpokryva@gmail.com>
2023-06-01 17:42:16 -0400

    hardcoded user id in context works

--- examples/user-authz/app/teams/[team]/page.tsx
+++ examples/user-authz/app/teams/[team]/page.tsx
@@ -17,8 +17,17 @@ const { db } = new Server({
 
 // in this example, team name will map to tenant (imagine name uniqueness is enforced in the tenant table)
 async function get(team: string) {
+  console.log(`team: ${team}`);
   const name = team.split('-').join(' ');
-  const [tenant] = await db('tenants').whereILike('name', name);
+  console.log(`name: ${name}`);
+  await db.raw('RESET nile.user_id');
+  await db.raw('RESET nile.tenant_id');
+  const [tenant] = (
+    await db.raw(`SELECT * from tenants where name ILIKE '${name}'`)
+  ).rows;
+  console.log(`tenant: ${JSON.stringify(tenant)}`);
+  await db.raw(`SET nile.tenant_id = '${tenant.id}'`);
+  await db.raw(`SET nile.user_id = '018878ca-9a22-7c2a-82ec-fd7c25de8578'`);
   const rawStops = await db
     .with('averages', (qb) => {
       qb.select(db.raw('AVG("end" - start) AS avg_duration')).from(
