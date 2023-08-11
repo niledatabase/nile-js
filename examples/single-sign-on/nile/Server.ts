be8f9e61882643b78f5b3456bf19e38e02c328a8
jrea <juzko.r@gmail.com>
2023-08-11 12:44:16 -0400

    chore: remove unnecessary config

--- examples/single-sign-on/nile/Server.ts
+++ examples/single-sign-on/nile/Server.ts
@@ -3,9 +3,6 @@ import Server from '@theniledev/server';
 const nile = Server({
   workspace: String(process.env.WORKSPACE),
   database: String(process.env.DATABASE),
-  api: {
-    basePath: String(process.env.NILE_BASE_PATH),
-  },
 });
 
 export const { api, db } = nile;
