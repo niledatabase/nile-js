e6f611f2f446eb74bbbcd3b442c9868213a62dd7
jrea <juzko.r@gmail.com>
2023-11-21 11:39:27 -0500

    chore: add debug test for connection failures

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['dotenv/config'],
 };
+
+process.env.USER = '';
+process.env.PASSWORD = '';
+process.env.WORKSPACE = '';
+process.env.DATABASE = '';
+process.env.USER_ID = '';
--- packages/server/src/db/db.test.ts
+++ packages/server/src/db/db.test.ts
@@ -1,12 +1,14 @@
+/* eslint-disable no-console */
 import { Config } from '../utils/Config';
 import { watchEvictPool } from '../utils/Event';
 
 import NileDatabase from './NileInstance';
 
+import DBManager from './index';
 import NileDB from './index';
 
 const properties = ['connections'];
-describe('db', () => {
+describe.skip('db', () => {
   it('has expected properties', () => {
     const db = new NileDB({
       workspace: 'workspace',
@@ -38,4 +40,76 @@ describe('db', () => {
       done();
     });
   });
+});
+
+const userId = process.env.USER_ID;
+
+describe('dbsync', () => {
+  it('does not work', (done) => {
+    const config = new Config({
+      workspace: String(process.env.WORKSPACE),
+      database: String(process.env.DATABASE),
+      db: {
+        connection: {
+          user: process.env.USER,
+          password: process.env.PASSWORD,
+          host: 'localhost',
+        },
+      },
+    });
+    const db = new DBManager(config);
+    // @ts-expect-error - test
+    const nile = db.getConnection(config).knex;
+    const manyQueries = [
+      nile('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+      nile('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+      nile('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+      nile('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+    ];
+    Promise.all(manyQueries).then((data) => {
+      console.log(data, 'first batch');
+    });
+
+    setTimeout(async () => {
+      const data = await Promise.all(manyQueries);
+      console.log(data, 'third batch');
+      done();
+    }, 100);
+    // @ts-expect-error - test
+    const nile2 = db.getConnection(config).knex;
+    const moreQueries = [
+      nile2('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+      nile2('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+      nile2('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+      nile2('tenants')
+        .select('tenants.id', 'tenants.name')
+        .join('users.tenant_users', 'tenants.id', '=', 'tenant_users.tenant_id')
+        .where('tenant_users.user_id', '=', userId),
+    ];
+
+    Promise.all(moreQueries).then((data) => {
+      console.log(data, 'second batch');
+    });
+  }, 50000);
 });
