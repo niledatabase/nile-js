86068ea4a67b95ebccf68e95c49ed262a049006a
jrea <juzko.r@gmail.com>
2024-04-09 11:24:41 -0400

    fix(repo): build esm better

--- packages/react/dts.config.js
+++ packages/react/dts.config.js
@@ -1,15 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const svgr = require('@svgr/rollup');
-const babel = require('@rollup/plugin-babel');
-const terser = require('rollup-plugin-terser');
 
 module.exports = {
   rollup(config) {
-    const basePlugins = config.plugins
-      .filter(Boolean)
-      .filter((config) => config.name !== 'babel')
-      .filter((config) => config.name !== 'terser');
-    config.plugins = [...basePlugins, babel(), svgr(), terser.terser()];
+    config.plugins.push(svgr());
     return config;
   },
 };
