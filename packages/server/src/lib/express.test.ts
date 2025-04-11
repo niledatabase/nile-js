5484b4eaa05c18b4ffabd7b849d2d5f49b62b4df
jrea <juzko.r@gmail.com>
2025-04-11 14:33:05 -0400

    fix(server): regex on express path cleaning

---
+++ packages/server/src/lib/express.test.ts
@@ -0,0 +1,49 @@
+import { Server } from '../Server';
+
+import { expressPaths } from './express';
+
+describe('express', () => {
+  it('cleans express paths', () => {
+    const nile = new Server();
+    const { paths } = expressPaths(nile);
+    expect(Object.keys(paths)).toEqual(['get', 'post', 'put', 'delete']);
+    expect(paths.delete).toEqual([
+      '/api/tenants/:tenantId/users/:userId',
+      '/api/tenants/:tenantId',
+    ]);
+    expect(paths.post).toEqual([
+      '/api/tenants/:tenantId/users',
+      '/api/signup',
+      '/api/users',
+      '/api/tenants',
+      '/api/auth/session',
+      '/api/auth/signin/:provider',
+      '/api/auth/reset-password',
+      '/api/auth/providers',
+      '/api/auth/csrf',
+      '/api/auth/callback/:provider',
+      '/api/auth/signout',
+    ]);
+    expect(paths.put).toEqual([
+      '/api/tenants/:tenantId/users',
+      '/api/users',
+      '/api/tenants/:tenantId',
+      '/api/auth/reset-password',
+    ]);
+    expect(paths.get).toEqual([
+      '/api/me',
+      '/api/tenants/:tenantId/users',
+      '/api/tenants',
+      '/api/tenants/:tenantId',
+      '/api/auth/session',
+      '/api/auth/signin',
+      '/api/auth/providers',
+      '/api/auth/csrf',
+      '/api/auth/reset-password',
+      '/api/auth/callback',
+      '/api/auth/signout',
+      '/api/auth/verify-request',
+      '/api/auth/error',
+    ]);
+  });
+});
