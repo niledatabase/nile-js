0efefabb31f3ea2623168b15fbcb31b345cfaa79
jrea <juzko.r@gmail.com>
2026-02-04 11:59:51 -0500

    fix(server): simplify getTokenFromCookie

--- packages/server/src/utils/fetch.ts
+++ packages/server/src/utils/fetch.ts
@@ -3,28 +3,22 @@ import { PartialContext } from '../types';
 import { TENANT_COOKIE, USER_COOKIE } from './constants';
 
 function getTokenFromCookie(headers: Headers, cookieKey: void | string) {
-  const cookie = headers.get('cookie')?.split('; ');
-  const _cookies: Record<string, string> = {};
-  if (cookie) {
-    for (const parts of cookie) {
-      const cookieParts = parts.split('=');
-      const _cookie = cookieParts.slice(1).join('=');
-      const name = cookieParts[0];
-      _cookies[name] = _cookie;
+  const cookieHeader = headers.get('cookie');
+  if (!cookieHeader) {
+    return null;
+  }
+
+  const cookies = cookieHeader.split('; ');
+  for (const parts of cookies) {
+    const cookieParts = parts.split('=');
+    const name = cookieParts[0];
+    const value = cookieParts.slice(1).join('=');
+
+    if (cookieKey && name === cookieKey) {
+      return value;
     }
   }
 
-  if (cookie) {
-    for (const parts of cookie) {
-      const cookieParts = parts.split('=');
-      const _cookie = cookieParts.slice(1).join('=');
-      const name = cookieParts[0];
-      _cookies[name] = _cookie;
-    }
-  }
-  if (cookieKey) {
-    return _cookies[cookieKey];
-  }
   return null;
 }
 export function getTenantFromHttp(headers: Headers, context?: PartialContext) {
